ab5c8bd6139474119e0573dcb8b963e4b346e113
zak <codedpills@gmail.com>
2019-08-28 15:15:30 +0000

    added wiggling animation when dice is rolling

--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,7 @@ export class Die extends Component {
     render() {
         return (
             <div className="die">
-                <i className={`die-face fas fa-dice-${this.props.face}`}></i>
+                <i className={`die-face fas fa-dice-${this.props.face} ${this.props.rolling && 'wiggling' }`}></i>
             </div>
         );
     }
